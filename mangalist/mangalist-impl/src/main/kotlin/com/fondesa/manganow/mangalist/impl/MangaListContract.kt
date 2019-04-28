6a9b02da22730dd6bb44cb9e4c5b2e1196fc8d24
antoniolig <giorgio.antonioli.dev@gmail.com>
2019-04-28 15:53:10 +0200

    Add contract and layout

---
+++ mangalist/mangalist-impl/src/main/kotlin/com/fondesa/manganow/mangalist/impl/MangaListContract.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019 Fondesa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.fondesa.manganow.mangalist.impl
+
+import com.fondesa.manganow.domain.manga.Manga
+import com.fondesa.manganow.mangalist.api.sortorder.SortOrder
+import com.fondesa.manganow.mangalist.api.sortorder.SortOrderList
+import com.fondesa.manganow.ui.api.mvp.BasePresenter
+
+object MangaListContract {
+
+    interface View {
+
+        fun showProgressIndicator()
+
+        fun hideProgressIndicator()
+
+        fun showListContainer()
+
+        fun hideListContainer()
+
+        fun showErrorForCause(cause: ErrorCause)
+
+        fun showZeroElementsView()
+
+        fun hideZeroElementsView()
+
+        fun showMangaList(mangaList: MangaList)
+
+        fun updatePageSize(pageSize: Int)
+
+        fun showSortOrdersView()
+
+        fun showSortOrders(sortOrders: SortOrderList, defaultSortOrder: SortOrder)
+    }
+
+    /**
+     * Specify the presenter methods for the manga list section linked to [View].
+     */
+    interface Presenter : BasePresenter {
+
+        fun pageEnded()
+
+        fun sortOrderSelected(sortOrder: SortOrder)
+
+        fun textSearched(text: String)
+
+        fun mangaClicked(manga: Manga)
+    }
+}
